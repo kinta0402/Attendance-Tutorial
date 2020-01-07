d9c41c41f35ffc18d91e051ecda9cebe8c2edd61
kinta0402 <goro0410@gmail.com>
2020-01-07 06:27:35 +0000

    Finish top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'static_pages/top'
+  # ↑ rails g で最初に作られたroutes ⇒ このままだと、Topページ【/】へアクセスした際、railsのwelcome画面に行ってしまうため、
+  # root urlのルーティングを下記に変更
   root 'static_pages#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
