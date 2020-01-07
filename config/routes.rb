019a68598ee7bdfe0c6bed5de3e015f4add64672
kinta0402 <goro0410@gmail.com>
2020-01-07 09:25:31 +0000

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # ↑ rails g で最初に作られたroutes ⇒ このままだと、Topページ【/】へアクセスした際、railsのwelcome画面に行ってしまうため、
   # root urlのルーティングを下記に変更
   root 'static_pages#top'
+  get '/signup', to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
