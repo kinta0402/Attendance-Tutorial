703f43cf05e2ae0fb10c66ffe6f4dff34b1ac557
kinta0402 <goro0410@gmail.com>
2020-01-09 08:11:25 +0000

    5.5.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,21 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    # @user = User.new(params[:user]) 現在のrailsのバージョンでは使用出来ない為下記のように記載
+    # user_params は private に定義【strong parameters】を使用
+    @user = User.new(user_params)
     if @user.save
       # 保存に成功した場合は、ここに記述した処理が実行されます。
     else
       render :new
     end
   end
+  
+  
+  private # 以下はstrong parameters !?
+  
+    def user_params # 5.5.3 参照
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
