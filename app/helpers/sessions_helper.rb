15efeab33d3e0edd6f85fe1fffc6978bf5fb768c
kinta0402 <goro0410@gmail.com>
2020-01-11 10:09:31 +0000

    6.3.1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+
+  # 引数に渡されたユーザーオブジェクトでログインします。
+  def log_in(user)
+    session[:user_id] = user.id  # session ﾒｿｯﾄﾞ【flash 変数に似ている】
+                                 # ﾛｸﾞｲﾝしたuser_id を sessionのuser_id に代入
+                                 # 例) session[:user_id] = '1'
+                                 # 例) flash.now[:danger] = '認証に失敗しました'
+  end
 end
