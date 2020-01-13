9f971cdf00869220a52646cd5fa734bad8273574
kinta0402 <goro0410@gmail.com>
2020-01-13 07:11:14 +0000

    6.3.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,18 @@
 module SessionsHelper
 
   # 引数に渡されたユーザーオブジェクトでログインします。
-  def log_in(user)
+  def log_in(user) # 引数のuser = sessions_controller の create に記載されている user か？
     session[:user_id] = user.id  # session ﾒｿｯﾄﾞ【flash 変数に似ている】
                                  # ﾛｸﾞｲﾝしたuser_id を sessionのuser_id に代入
                                  # 例) session[:user_id] = '1'
                                  # 例) flash.now[:danger] = '認証に失敗しました'
+  end
+  
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  # 現在ログインしているユーザーの値を取得
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])  # この式はややこしい 6.3.2に記載
+    end
   end
 end
