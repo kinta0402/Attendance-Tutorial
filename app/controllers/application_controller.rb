16b7234955c1923b14222b011257edc74b8d6578
kinta0402 <goro0410@gmail.com>
2020-01-29 07:18:00 +0000

    10.4途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,19 +6,21 @@ class ApplicationController < ActionController::Base
                                                # %w{} = リテラル表記 10.3
                                                # ["日", "月", "火", "水", "木", "金", "土"]の配列と同じように使えます。
                                                
-  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。# ややこしい10.3
+  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。# ややこしい10.3 10.4
   def set_one_month 
-    @first_day = Date.current.beginning_of_month
+    @first_day = params[:date].nil? ? 
+    Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
     # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day)
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
 
     unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
       ActiveRecord::Base.transaction do # トランザクションを開始します。
         # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
 
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
