d5edca8137bd0ccb4b3e7ab5a8d31e4c32e3520c
kinta0402 <goro0410@gmail.com>
2020-01-30 09:42:25 +0000

    10.6.2

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,7 @@
 class AttendancesController < ApplicationController
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。" # 定数(全て大文字)
 
-  def update
+  def update #10.5.3
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
@@ -8,7 +9,13 @@ class AttendancesController < ApplicationController
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
-        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil? # 10.6.2
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
       end
     end
     redirect_to @user
