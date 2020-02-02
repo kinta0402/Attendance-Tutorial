ac16c21dbc5dc21fcfc9259098dd70a4b45daa90
kinta0402 <goro0410@gmail.com>
2020-02-02 07:57:44 +0000

    Finish edit-attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,8 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  #勤怠情報に関して、管理者 または ログインユーザー本人であれば実行可能
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
 
   
@@ -45,9 +47,23 @@ class AttendancesController < ApplicationController
   end
 
   private
+  
+    # ストロングパラメーター
+    
     # 1ヶ月分の勤怠情報を扱います。
     def attendances_params # 11.2 
       params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
     end
-  
+
+
+    # beforeフィルター
+    
+    # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
+    end  
 end
